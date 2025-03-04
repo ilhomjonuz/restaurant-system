ded94b23d1a7248cd2078285707db45fb2903ee4
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2025-03-04 22:05:52 +0500

    basic docker configuration

--- bot/data/config.py
+++ bot/data/config.py
@@ -5,3 +5,4 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS", default=[])
+WEB_APP_URL = env.str("WEB_APP_URL")
--- bot/keyboards/reply/menu.py
+++ bot/keyboards/reply/menu.py
@@ -1,13 +1,15 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import WebAppInfo
 
+from data import config
+
 
 async def user_menu():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
     markup.add(
         KeyboardButton(
             text="Online menu",
-            web_app=WebAppInfo(url="https://2ac4-213-230-76-167.ngrok-free.app/products/products-list/")
+            web_app=WebAppInfo(url=config.WEB_APP_URL)
         )
     )
     return markup
